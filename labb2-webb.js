9886fc37826475d44ddaf07a6a73d5f4ed20e36c
filip <filip.christofferson@gmail.com>
2019-04-26 14:41:29 +0200

    Start working with score system

--- labb2-webb.js
+++ labb2-webb.js
@@ -1,142 +1,190 @@
    const store = new Vuex.Store({
-       state: {
-           dices: [{
-                   name: 'diceOne',
-                   roll: 0,
-                   hold: false
+               state: {
+                   dices: [{
+                           name: 'diceOne',
+                           roll: 1,
+                           hold: false
+                       },
+                       {
+                           name: 'diceTwo',
+                           roll: 0,
+                           hold: false
+                       },
+                       {
+                           name: 'diceThree',
+                           roll: 0,
+                           hold: false
+                       },
+                       {
+                           name: 'diceFour',
+                           roll: 0,
+                           hold: false
+                       },
+                       {
+                           name: 'diceFive',
+                           roll: 0,
+                           hold: false
+                       }
+                   ],
+                   scoreTable: [{
+                       name: 'ones',
+                       score: 0,
+                       status: false
+                   }, {
+                       name: 'twos',
+                       score: 0,
+                       status: false
+                   }, {
+                       name: 'threes',
+                       score: 0,
+                       status: false
+                   }, {
+                       name: 'fours',
+                       score: 0,
+                       status: false
+                   }, {
+                       name: 'fives',
+                       score: 0,
+                       status: false
+                   }, {
+                       name: 'sixes',
+                       score: 0,
+                       status: false
+                   }]
                },
-               {
-                   name: 'diceTwo',
-                   roll: 0,
-                   hold: false
-               },
-               {
-                   name: 'diceThree',
-                   roll: 0,
-                   hold: false
-               },
-               {
-                   name: 'diceFour',
-                   roll: 0,
-                   hold: false
-               },
-               {
-                   name: 'diceFive',
-                   roll: 0,
-                   hold: false
-               }
-           ]
-       },
-       mutations: {
-           selectDice(state, index) {
-               state.dices[index].hold = !state.dices[index].hold
-               
-           },
-           
-       }
-   })
+               mutations: {
+                   selectDice(state, index) {
+                       state.dices[index].hold = !state.dices[index].hold
+                   },
+                   diceCombos(state) {
+                       for (let i = 0; i < state.dices.length; i++) {
+                           const element = state.dices[i];
+                           if (state.dices[i].roll === 1) {
+                               state.scoreTable[0].score += 1
+                           }
+                           if (state.dices[i].roll === 2) {
+                               state.scoreTable[1].score += 2
+                           }
+                           if (state.dices[i].roll === 3) {
+                               state.scoreTable[2].score += 3
+                           }
+                           if (state.dices[i].roll === 4) {
+                            state.scoreTable[3].score += 4
+                        }
+                        if (state.dices[i].roll === 5) {
+                            state.scoreTable[4].score += 5
+                        }
+                        if (state.dices[i].roll === 6) {
+                            state.scoreTable[5].score += 6
+                        }
+                        }
+                    }
+                }
+                       })
 
-   Vue.component('rolldicecomponent', {
-       props:[],
-       data:
-       timesRolled = 0,
-       template: `
-       <a class="button" v-on:click="diceRoll">Click me to roll dice</a>
+                   Vue.component('rolldicecomponent', {
+                       props: [],
+                       data: timesRolled = 0,
+                       template: `
+       <div>
+       <a class="button" @click="diceRoll">Click me to roll dice</a>
+       </div>
        `,
-       methods: {
-       diceRoll: function () {
+                       methods: {
 
-        for (let i = 0; i < this.$store.state.dices.length; i++) {
+                           resetScore: function () {
+                               for (let i = 0; i < this.$store.state.scoreTable.length; i++) {
+                                   const element = this.$store.state.scoreTable[i];
 
-            const element = this.$store.state.dices[i];
-            if (this.$store.state.dices[i].hold == false) {
-                this.$store.state.dices[i].roll = Math.floor(Math.random() * 6) + 1
-            }
+                                   this.$store.state.scoreTable[i].score = 0;
 
-        }
-        timesRolled += 1
-        if (timesRolled === 4){
-            timesRolled = 1
-        }
-        console.log(timesRolled)
-    }
-   }
-})
+                               }
+                           },
+                           diceRoll: function () {
+                               this.resetScore()
+                               for (let i = 0; i < this.$store.state.dices.length; i++) {
 
-    Vue.component('dice', {
-       props: ['dices', 'dice', 'index',],
-       template: `
+                                   const element = this.$store.state.dices[i];
+                                   if (this.$store.state.dices[i].hold == false) {
+                                       this.$store.state.dices[i].roll = Math.floor(Math.random() * 6) + 1
+
+                                   }
+
+                               }
+                               store.commit('diceCombos')
+                               timesRolled += 1
+                               if (timesRolled === 4) {
+                                   timesRolled = 1
+                               }
+                               //console.log(timesRolled)
+                           },
+                       },
+
+                   })
+
+                   Vue.component('dice', {
+                       props: ['dices', 'dice', 'index', ],
+                       template: `
             <div>
                 <img v-bind:src="generateImage(index)" @click="changeHold(index)" alt="">
             </div>
         `,
-       methods: {
-           //Loops through the store dice objects and gives them all a new random value from 1 - 6
-           diceRoll: function () {
+                       methods: {
+                           //Loops through the store dice objects and gives them all a new random value from 1 - 6
 
-               for (let i = 0; i < this.$store.state.dices.length; i++) {
-
-                   const element = this.$store.state.dices[i];
-                   if (this.$store.state.dices[i].hold == false) {
-                       this.$store.state.dices[i].roll = Math.floor(Math.random() * 6) + 1
-                   }
-               }
+                           changeHold: function (index) {
+                               store.commit('selectDice', index)
 
 
-           },
-           changeHold: function (index) {
-               store.commit('selectDice', index)
+                           },
+                           //Generates an image of the dice depending on the value that is randomly generated 
+                           generateImage: function (i) {
+
+                               if (this.$store.state.dices[i].roll === 1) {
+                                   return "http://i.imgur.com/6knk862.png";
+                               }
+
+                               if (this.$store.state.dices[i].roll === 2) {
+                                   return "http://i.imgur.com/ik7dK9D.png";
+                               }
+
+                               if (this.$store.state.dices[i].roll === 3) {
+                                   return "http://i.imgur.com/sh0H0td.png";
+                               }
+
+                               if (this.$store.state.dices[i].roll === 4) {
+                                   return "http://i.imgur.com/1GPkhq3.png";
+                               }
+
+                               if (this.$store.state.dices[i].roll === 5) {
+                                   return "http://i.imgur.com/bINitmy.png";
+                               }
+
+                               if (this.$store.state.dices[i].roll === 6) {
+                                   return "http://i.imgur.com/6qXMSrt.png";
+                               }
+                               return "http://i.imgur.com/6knk862.png";
+                           }
+                       },
+                       computed: {
+
+                       }
+                   })
+
+                   const app = new Vue({
+                       el: '#app',
+                       store,
+                       data: {
+
+                       },
+                       computed: {
+                           dices() {
+                               return store.state.dices;
+                           }
+                       },
+                       methods: {
+
+                       }
 
 
-           },
-           //Generates an image of the dice depending on the value that is randomly generated 
-           generateImage: function (i) {
-
-               if (this.$store.state.dices[i].roll === 1) {
-                   return "http://i.imgur.com/6knk862.png";
-               }
-
-               if (this.$store.state.dices[i].roll === 2) {
-                   return "http://i.imgur.com/ik7dK9D.png";
-               }
-
-               if (this.$store.state.dices[i].roll === 3) {
-                   return "http://i.imgur.com/sh0H0td.png";
-               }
-
-               if (this.$store.state.dices[i].roll === 4) {
-                   return "http://i.imgur.com/1GPkhq3.png";
-               }
-
-               if (this.$store.state.dices[i].roll === 5) {
-                   return "http://i.imgur.com/bINitmy.png";
-               }
-
-               if (this.$store.state.dices[i].roll === 6) {
-                   return "http://i.imgur.com/6qXMSrt.png";
-               }
-               return "http://i.imgur.com/6knk862.png";
-           }
-       },
-       computed: {
-
-       }
-   })
-
-   const app = new Vue({
-       el: '#app',
-       store,
-       data: {
-
-       },
-       computed: {
-           dices() {
-               return store.state.dices;
-           }
-       },
-       methods: {
-        
-    }
-
-
-   })
+                   })
